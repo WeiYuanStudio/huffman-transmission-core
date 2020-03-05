77becabb63deac76b202c4b98782aef99aa917c1
WeiYuanStudio <WeiYuanStudio@gmail.com>
2020-03-05 15:16:34 +0800

    :recycle: Refactor HuffmanBinaryMsgDecoder

--- src/main/java/HuffmanUtil/HuffmanBinaryMsgDecoder.java
+++ src/main/java/HuffmanUtil/HuffmanBinaryMsgDecoder.java
@@ -4,33 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HuffmanBinaryMsgDecoder {
-    private Map<Character, String> charDict; //K:字符，V:01哈夫曼字典
-    private Map<String, Character> reverseCharDict; //K:01哈夫曼，V:字符 反转字典
-    private String binaryContent; //String 格式哈夫曼01代码
-    private StringBuilder messageContent;
+    private Map<String, Character> reverseCharDict = new HashMap<>(); //K:01哈夫曼，V:字符 反转字典
+    private String binaryContent; //String格式哈夫曼01代码
+    private StringBuilder messageContent = new StringBuilder(); //存放解码完毕的消息
 
     public HuffmanBinaryMsgDecoder(Map<Character, String> charDict, String binaryContent) {
-        this.charDict = charDict;
+        charDict.forEach((key, value) -> this.reverseCharDict.put(value, key)); //哈夫曼字典键值反转
         this.binaryContent = binaryContent;
-        this.reverseCharDict = new HashMap<>();
-        this.charDict.forEach((key, value) -> {
-            this.reverseCharDict.put(value, key);
-        });
-        this.messageContent = new StringBuilder();
     }
 
+    /**
+     * 通过反转后的字典将String格式哈夫曼01代码解码
+     *
+     * @return 返回解码后字符串
+     */
     public String decode() {
-        StringBuilder huffmanBuffer = new StringBuilder();
+        StringBuilder huffmanBuffer = new StringBuilder(); //用于暂时存放哈夫曼01代码
         binaryContent.chars().forEach(_char -> {
-            huffmanBuffer.append((char) _char);
-            if (reverseCharDict.get(huffmanBuffer.toString()) != null) {
-                messageContent.append(reverseCharDict.get(huffmanBuffer.toString()));
-                huffmanBuffer.delete(0, huffmanBuffer.length());
+            huffmanBuffer.append((char) _char); //逐位添加哈夫曼01代码
+            if (reverseCharDict.get(huffmanBuffer.toString()) != null) { //是否找到了哈夫曼01代码对应的字符
+                messageContent.append(reverseCharDict.get(huffmanBuffer.toString())); //将该字符存放
+                huffmanBuffer.delete(0, huffmanBuffer.length()); //清空暂存
             }
-            System.out.println(huffmanBuffer.toString());
-            System.out.println(reverseCharDict.get(huffmanBuffer.toString()));
         });
-        System.out.println(messageContent);
-        return null;
+        return messageContent.toString();
     }
 }
