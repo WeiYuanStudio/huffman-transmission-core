5a61129a8e94fb88201c35a768af87ffaf8b1b57
WeiYuanStudio <WeiYuanStudio@gmail.com>
2020-03-07 18:08:43 +0800

    :bug: fix BinaryToByte util array container length bug

--- src/main/java/SocketUtil/Binary/BinaryToByte.java
+++ src/main/java/SocketUtil/Binary/BinaryToByte.java
@@ -1,13 +1,15 @@
 package SocketUtil.Binary;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * String格式二进制序列转至Byte
  */
 public class BinaryToByte {
     private String binarySequence;
-    private ByteBuffer byteBuffer = ByteBuffer.allocate(1000);
+    private List<Byte> byteBuffer = new ArrayList<>();
 
     public BinaryToByte(String binarySequence) {
         this.binarySequence = binarySequence;
@@ -22,20 +24,23 @@ public class BinaryToByte {
             if (binaryCodeBuffer.length() == 8) {
                 byte tempByte = (byte) Integer.parseInt(binaryCodeBuffer.toString(), 2); //编码
                 binaryCodeBuffer.delete(0, binaryCodeBuffer.length()); //清空二进制String缓存区
-                byteBuffer.put(tempByte); //将编码后的字节数据存放
+                byteBuffer.add(tempByte); //将编码后的字节数据存放
             }
         });
-
         //将剩余不足8bit的数据补充到8bit
         if (binaryCodeBuffer.length() != 0) {
             do {
                 binaryCodeBuffer.append('0');
-            } while (binaryCodeBuffer.length() == 8);
+            } while (binaryCodeBuffer.length() != 8);
             byte tempByte = (byte) Integer.parseInt(binaryCodeBuffer.toString(), 2); //编码
             binaryCodeBuffer.delete(0, binaryCodeBuffer.length()); //清空二进制String缓存区
-            byteBuffer.put(tempByte); //将编码后的字节数据存放
+            byteBuffer.add(tempByte); //将编码后的字节数据存放
         }
 
-        return byteBuffer.array(); //返回byte数组
+        byte[] byteBufferArray = new byte[byteBuffer.size()];
+        for (int i = 0; i < byteBuffer.size(); ++i) {
+            byteBufferArray[i] = byteBuffer.get(i);
+        }
+        return byteBufferArray; //返回byte数组
     }
 }
